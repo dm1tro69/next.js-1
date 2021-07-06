e83bbba6e5fabcdf6d88622caebebc487a4de8f5
dm1tro69 <dimolg22@gmail.com>
2021-07-07 00:02:00 +0300

    add classnames

---
+++ components/Button/Button.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./Button.module.css";
+import { ButtonProps } from "./Button.props";
+
+// @ts-ignore
+import cn from "classnames";
+
+const Button = ({
+  appearance,
+  children,
+  className,
+  ...props
+}: ButtonProps): JSX.Element => {
+  return (
+    <button
+      className={cn(styles.button, className, {
+        [styles.primary]: appearance === "primary",
+        [styles.ghost]: appearance === "ghost",
+      })}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
