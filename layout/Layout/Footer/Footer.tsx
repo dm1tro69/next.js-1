78b0101624019b898fc8d0b24f806db100b95cca
dm1tro69 <dimolg22@gmail.com>
2021-07-08 22:27:02 +0300

    add .env.local

--- layout/Layout/Footer/Footer.tsx
+++ layout/Layout/Footer/Footer.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import styles from "./Footer.module.css";
 import { FooterProps } from "./Footer.props";
+import { format } from "date-fns";
 
 // @ts-ignore
 import cn from "classnames";
 
-const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div>OwlTop © 2020 - {format(new Date(), "yyyy")} Все права защищены</div>
+      <a href="#">Пользовательское соглашение</a>
+      <a href="#">Политика конфиденциальности</a>
+    </footer>
+  );
 };
 
 export default Footer;
