0236141c83e465e36bbdfa8b92d05aac18c53d05
dm1tro69 <dimolg22@gmail.com>
2021-07-09 15:33:19 +0300

    add context

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,11 @@ import Tag from "../components/Tag/Tag";
 import { useState } from "react";
 import Rating from "../components/Rating/Rating";
 import Layout, { withLayout } from "../layout/Layout/Layout";
+import { GetStaticProps } from "next";
+import axios from "axios";
+import { MenuItem } from "../interfaces/menu.interface";
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
   const [counter, setCounter] = useState<number>(0);
   return (
     <>
@@ -33,7 +36,30 @@ function Home(): JSX.Element {
         textRed
       </Tag>
       <Rating rating={4} isEditable={true} />
+      <ul>
+        {menu.map((m) => {
+          return <li key={m._id.secondCategory}>{m._id.secondCategory}</li>;
+        })}
+      </ul>
     </>
   );
 }
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find",
+    { firstCategory }
+  );
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
+};
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
